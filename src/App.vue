6b2d4563f7b5711fb3383f56cda8f47cfffe0738
David Annebicque <david.annebicque@gmail.com>
2021-11-17 10:43:54 +0100

    Tests sur chaque note

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,52 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
+      |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
 
     <ul>
-      <li v-for="(note, matiere) in notes" :key="matiere">Note sur {{matiere}} : {{ note }}</li>
-    <li>Moyenne des notes : {{ moyenneCalculee }}</li>
+      <li v-for="(note, matiere) in notes" :key="matiere">
+        Note sur {{ matiere }} : {{ note }} =>
+        <span v-if="note > 10">{{ matiere }} validé</span>
+        <span v-else><b>{{ matiere }} non validé</b></span>
+      </li>
+      <li>Moyenne des notes : {{ moyenneCalculee }}</li>
       <li v-if="moyenneCalculee >10">Vous êtes admis</li>
       <li v-else>Play again !</li>
     </ul>
 
-  <input type="text" v-model.number="notes.Symfony">
+    <input type="text" v-model.number="notes.Symfony">
 
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'App',
-    data: () => {
-      return {
-        notes: {
-          "Symfony": 12,
-          "Réseau": 14,
-          "Intégration": 8,
-          "Maths": 13,
-          "Anglais": 12
-        }
-      }
-    },
-    computed: {
-      moyenneCalculee() {
-        let somme = 0;
-        for (let matiere in this.notes) {
-          somme += this.notes[matiere];
-        }
-        return somme / Object.keys(this.notes).length;
+export default {
+  name: 'App',
+  data: () => {
+    return {
+      notes: {
+        'Symfony': 12,
+        'Réseau': 14,
+        'Intégration': 8,
+        'Maths': 13,
+        'Anglais': 12
       }
     }
+  },
+  computed: {
+    moyenneCalculee () {
+      let somme = 0
+      for (let matiere in this.notes) {
+        somme += this.notes[matiere]
+      }
+      return somme / Object.keys(this.notes).length
+    }
   }
+}
 </script>
 
 <style>
